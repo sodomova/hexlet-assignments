6a5b3a5fb02e51fd87a83974b1c5d7fe24d6947c
@hexlet/cli <support@hexlet.io>
2022-11-04 13:44:46 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -9,14 +9,29 @@ import org.junit.jupiter.api.Test;
 class AppTest {
 
     @Test
-    void testTake() {
-        // BEGIN
+    void testTake1() {
         List<Integer> list = List.of(1, 2, 3, 4, 5);
-        int n = 3;
-        List<Integer> result = App.take(list, n);
         List<Integer> correctAnswer = List.of(1, 2, 3);
+        List<Integer> result = App.take(list, 3);
 
         assertThat(result).isEqualTo(correctAnswer);
-        // END
+    }
+
+    @Test
+    void testTake2() {
+        List<Integer> list = new ArrayList<>();
+        List<Integer> correctAnswer = new ArrayList<>();
+        List<Integer> result = App.take(list, 3);
+
+        assertThat(result).isEqualTo(correctAnswer);
+    }
+
+    @Test
+    void testTake3() {
+        List<Integer> list = List.of(1, 2, 3, 4, 5);
+        List<Integer> correctAnswer = List.of(1, 2, 3, 4, 5);
+        List<Integer> result = App.take(list, 8);
+
+        assertThat(result).isEqualTo(correctAnswer);
     }
 }
